177a0e9f9b2620de9bcbf5012086ef9f346268bf
Jannape <yamileth_2005@hotmail.com>
2018-08-13 18:21:19 -0500

    agregando viewlistSities

--- src/controller/main.js
+++ src/controller/main.js
@@ -4,6 +4,7 @@ viewListSities=(sitie,foodsContainer)=>{
 
   
           console.log('coincido con el tipo ');
+          foodsContainer.innerHTML = "";
           foodsContainer.innerHTML += `
           <div  class="card">
           <img class="card-img-top" src="${sitie.url}" alt="Card image cap"> </div> `
